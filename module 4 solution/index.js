3e2968a141bc8ac0ab329c9876472e35c215c113
samueledem113 <139284030+samueledem113@users.noreply.github.com>
2024-06-06 18:16:12 +0000

    index.js

--- module 4 solution/index.js
+++ module 4 solution/index.js
@@ -1,5 +1,5 @@
 // Array of names
-let names = ["John", "Paul", "George", "Ringo", "Jane", "Emily", "Jack", "Anna"];
+let names = ["John", "Paul", "George", "Ringo", "Jane", "Emily", "Yaakov", "Paula"];
 
 // Loop over the array
 for (let i = 0; i < names.length; i++) {
@@ -8,8 +8,8 @@ for (let i = 0; i < names.length; i++) {
   
   // Check the first letter of the name
   if (name.charAt(0).toLowerCase() === 'j') {
-    console.log("Goodbye " + name);
+    console.log("Goodbye " + yaakov);
   } else {
-    console.log("Hello " + name);
+    console.log("Hello " + yaakov);
   }
 }

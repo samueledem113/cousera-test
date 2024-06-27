a94d4839d0c5885020b2d32ea779b6b9fcd8d302
samueledem113 <139284030+samueledem113@users.noreply.github.com>
2024-06-27 13:51:01 +0000

    Update script.js

--- module 4 solution/script.js
+++ module 4 solution/script.js
@@ -1,15 +1,10 @@
 // Array of names
-let names = ["John", "Paul", "George", "Ringo", "Jane", "Emily", "Yaakov", "Paula"];
+var speakWord = "Good Bye";
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+var speakWord = "Hello";
 
-// Loop over the array
-for (let i = 0; i < names.length; i++) {
-  // Get the current name
-  let name = names[i];
-  
-  // Check the first letter of the name
-  if (name.charAt(0).toLowerCase() === 'j') {
-    console.log("Goodbye " + yaakov);
-  } else {
-    console.log("Hello " + yaakov);
-  }
+function speak(name) {
+  console.log(speakWord + " " + name);
 }
